4c07250283441d6c00ae6eb44c651f38bc3d27e3
akhandelwal1982 <35450173+akhandelwal1982@users.noreply.github.com>
2018-02-02 02:52:29 +0530

    Update Sentiment_Analysis.R

--- Sentiment_Analysis.R
+++ Sentiment_Analysis.R
@@ -1,4 +1,22 @@
-text <- readLines("https://raw.githubusercontent.com/akhandelwal1982/DAM/master/nokia.txt")
+Clean_Text <- function(x){   # x = a text doc
+   require("tm")    # always invoke required libraries
+  
+  x  =  gsub("<.*?>", " ", x)               # regex for removing HTML tags
+  x  =  iconv(x, "latin1", "ASCII", sub="") # Keep only ASCII characters
+  x  =  gsub("[^[:alnum:]]", " ", x)        # keep only alpha numeric 
+  x  =  tolower(x)                          # convert to lower case characters
+  x  =  removeNumbers(x)                    # removing numbers
+  x  =  stripWhitespace(x)                  # removing white space
+  x  =  gsub("^\\s+|\\s+$", "", x)          # remove leading and trailing white space
+  
+  # removing standard english stopwords like 'the', 'an' etc
+  stopwords = tm::stopwords('english')      # tm's inbuilt stopword list; 'tokenizer' too has a stopwords() func, hence 'tm::'
+  
+  x  =  removeWords(x,stopwords)            # removing stopwords created above
+  x  =  stripWhitespace(x)                  # removing white space
+  #  x  =  stemDocument(x)                   # can stem doc if needed. Will need Snowball libraries if so.
+  
+  return(x) }
 
 emotion_func <- function(text){
   if (!require(tidytext)) {install.packages("tidytext")}
